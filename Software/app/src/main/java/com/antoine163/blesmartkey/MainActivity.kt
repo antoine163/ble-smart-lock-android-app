98b7fc3c506dc63aee9bd015da3d93af675c6833
antoine163 <antoine.maleyrie@gmail.com>
2024-08-04 14:44:21 +0200

    Refactor: Request Bluetooth permissions on app launch
    This commit refactors the Bluetooth permission handling to request all necessary permissions directly on app launch.
    
    The `
    checkPermission` function has been removed and the permission request logic is now streamlined within the `onCreate` method. This ensures that the app requests the required permissions as soon as it starts, simplifying the permission handling process.

--- Software/app/src/main/java/com/antoine163/blesmartkey/MainActivity.kt
+++ Software/app/src/main/java/com/antoine163/blesmartkey/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.antoine163.blesmartkey
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,47 +9,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
 import com.antoine163.blesmartkey.ui.theme.BleSmartKeyTheme
 
 class MainActivity : ComponentActivity() {
+
+    // Bluetooth permissions required for Android 12 (API 31) and above
+    @RequiresApi(Build.VERSION_CODES.S)
+    private val bluetoothPermissions = arrayOf(
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_ADMIN,
+    )
+
+    private val requestBluetoothPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("learnBle", "${it.key} = ${it.value}")
+            }
+        }
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        checkPermission()
+        requestBluetoothPermissions.launch(bluetoothPermissions) // Request permissions
         setContent {
             BleSmartKeyTheme {
                 BleSmartKeyApp()
             }
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun checkPermission() {
-        val permissions: Array<String> = arrayOf(
-            Manifest.permission.BLUETOOTH_SCAN,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_ADMIN,
-        )
-
-        permissions.forEach { permission ->
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                requestPermissionLauncher.launch(permissions)
-                return
-            }
-        }
-    }
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions: Map<String, Boolean> ->
-        Log.i("learnBle", "permissions:${permissions}")
     }
 }
