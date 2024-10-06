2f355d584bc1759d8489aa34b8405337c5c00f1b
antoine163 <antoine.maleyrie@gmail.com>
2024-10-06 17:17:05 +0200

    chore: Update Gradle and AGP versions
    - Updated Gradle to 8.10.2
    - Updated Android Gradle Plugin (AG
    P) to 8.8.0-alpha05
    - Increased minimum SDK version to 33

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.antoine163.blesmartkey"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
