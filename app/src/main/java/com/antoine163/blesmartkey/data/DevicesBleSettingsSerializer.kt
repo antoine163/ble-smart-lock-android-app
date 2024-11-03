98043438c97619a458f155e6878807e3bbcaaad3
antoine163 <antoine.maleyrie@gmail.com>
2024-11-03 21:07:47 +0100

    docs: Add documentation to DevicesBleSettingsSerializer
    This commit adds documentation to the `DevicesBleSettingsSerializer` class to improve code readability and understanding.
    
    - Added comprehensive documentation for the `defaultValue`, `readFrom`, and `writeTo` functions, explaining their purpose, parameters, and potential exceptions.
    - Added a brief description of the serializer's role in handling `DevicesBleSettings` data.

--- app/src/main/java/com/antoine163/blesmartkey/data/DevicesBleSettingsSerializer.kt
+++ app/src/main/java/com/antoine163/blesmartkey/data/DevicesBleSettingsSerializer.kt
@@ -15,7 +15,26 @@ import java.io.OutputStream
  * If an error occurs during parsing, a [CorruptionException] is thrown.
  */
 object DevicesBleSettingsSerializer : Serializer<DevicesBleSettings> {
+
+    /**
+     * The default value for this setting.
+     * This value will be used if no setting is found in the persistent storage.
+     *
+     * In this case, it returns a default instance of [DevicesBleSettings].
+     */
     override val defaultValue: DevicesBleSettings = DevicesBleSettings.getDefaultInstance()
+
+    /**
+     * Reads and parses a [DevicesBleSettings] object from the provided [InputStream].
+     *
+     * This function attempts to parse the input stream as a serialized [DevicesBleSettings] protobuf message.
+     * If the parsing is successful, it returns the parsed [DevicesBleSettings] object.
+     * If the input stream is corrupted or does not contain a valid protobuf message, a [CorruptionException] is thrown.
+     *
+     * @param input The [InputStream] to read from.
+     * @return The parsed [DevicesBleSettings] object.
+     * @throws CorruptionException If the input stream is corrupted or does not contain a valid protobuf message.
+     */
     override suspend fun readFrom(input: InputStream): DevicesBleSettings {
         try {
             return DevicesBleSettings.parseFrom(input)
@@ -24,5 +43,14 @@ object DevicesBleSettingsSerializer : Serializer<DevicesBleSettings> {
         }
     }
 
+    /**
+     * Writes the [DevicesBleSettings] object to the given [OutputStream].
+     *
+     * This function serializes the [DevicesBleSettings] object and writes it to the provided output stream.
+     * It delegates the actual writing process to the `writeTo` function of the [DevicesBleSettings] object.
+     *
+     * @param t The [DevicesBleSettings] object to be written.
+     * @param output The [OutputStream] to write the object to.
+     */
     override suspend fun writeTo(t: DevicesBleSettings, output: OutputStream) = t.writeTo(output)
 }
